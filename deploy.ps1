093ea9db8d6ad4db98e5565e0987f6694ca18baf
Christopher Ng <facboy@gmail.com>
2023-01-05 09:56:36 +0000

    Add build/deploy PowerShell scripts

---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+gsudo {
+    cd $args[0]
+
+    $BuildDir = "out\build\x64-ovpn3\Release\\"
+    $DestDir = "C:\Program Files\OpenVPN\bin\\"
+
+    robocopy "$BuildDir" "$DestDir" "json-c.dll" "openvpn-gui.exe" "test_plap.exe" /w:5
+} -args $PSScriptRoot
